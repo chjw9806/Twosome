ab5e9b9173bc8c47b7b2166bba9dc729478f8dd1
82103 <chjw9806@naver.com>
2021-06-06 22:41:39 +0900

    modify

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,69 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnTwoCaramel, btnTwoDalgona, btnTwoAmericano, btnTwoColdbrew, btnTwoCafemoca, btnTwoSignature, btnTwoTonic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTwoCaramel = findViewById(R.id.btnTwoCaramel);
+        btnTwoDalgona = findViewById(R.id.btnTwoDalgona);
+        btnTwoAmericano = findViewById(R.id.btnTwoAmericano);
+        btnTwoColdbrew = findViewById(R.id.btnTwoColdbrew);
+        btnTwoCafemoca = findViewById(R.id.btnTwoCafemoca);
+        btnTwoSignature = findViewById(R.id.btnTwoSignature);
+        btnTwoTonic = findViewById(R.id.btnTwoTonic);
+
+        btnTwoCaramel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnTwoDalgona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnTwoAmericano.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnTwoColdbrew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnTwoCafemoca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnTwoSignature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnTwoTonic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
